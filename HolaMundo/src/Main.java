d5b76618713d35a52168dfe702a4338f2f3e36c9
ANUAR-LAPTOP\anuar <akhan@decroly.com>
2025-09-26 10:57:49 +0200

    Primeros ejemplos Java clase 26-9-2025 (variables, identificadores, etc.)

---
+++ HolaMundo/src/Main.java
@@ -0,0 +1,35 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        // Esto es un comentario de una linea
+
+        /*
+        sadasdsad
+        dfdsfsdf
+        sdfsdfsdf
+        dsfsdf
+         */
+
+        /**
+         * @Author: Anuar Khan
+         * @params: none
+         */
+
+        System.out.printf("Hello and welcome!");
+
+        int num = -5;
+
+        System.out.println(num);
+
+        num++; // num = num + 1;
+
+        boolean resp = (num > 0 || num < 10);
+
+        Math.pow(3,8);
+
+
+    }
+}
---
+++ ejemplo2/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        int num = 23;
+
+        String nombre = "Anuar " + "Khan Ali";
+
+        char caract = 'c';
+
+        System.out.println(nombre);
+
+        String texto = " ddslñf-ldsmflÑsdmfmsdfm,sdmfsdmdf,,sdmf,msd.fmsd,fms"
+                + ".d,fm.,sdmdf.,sdmdf,sdmf,smf,msd.,fdmsd,.fm.,s,dmfsdmf.,smd,mf.,sdmf,s,dmdf,sdnfnfn";
+    
+    }
+}
