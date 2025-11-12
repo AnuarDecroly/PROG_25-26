0e91b21dd26d5addeffdb69289513e9eb1734308
ANUAR-LAPTOP\anuar <akhan@decroly.com>
2025-11-12 10:11:46 +0100

    Ejemplo biblio completo

--- BiblioDAW/src/Biblioteca.java
+++ BiblioDAW/src/Biblioteca.java
@@ -86,7 +86,101 @@ public class Biblioteca {
         return resultado;
     }
 
-    
+    public Libro getLibroByISBN(String ISBN) {
+        int index = -1;
+        for(int i = 0; i < this.nLibrosActuales; i++) {
+            if (this.libros[i] != null) {
+                if (this.libros[i].getISBN().equalsIgnoreCase(ISBN)) {
+                    index = i;
+                    break; //para eliminar la primera ocurrencia
+                }
+            }
+        }
+        if(index == -1) {
+            return null;
+        }
+        else{
+            return this.libros[index];
+        }
+
+//        //Otra Opcion
+//        Libro libro = null;
+//        for(int i = 0; i < this.nLibrosActuales; i++) {
+//            if (this.libros[i] != null) {
+//                if (this.libros[i].getISBN().equalsIgnoreCase(ISBN)) {
+//                    libro = libros[i];
+//                    break;
+//                }
+//            }
+//        }
+//        return libro;
+
+    }
+
+    public boolean eliminarLibro(Libro libro) {
+        boolean resultado = false;
+
+        if(libro != null) {
+            int index = -1;
+            for(int i = 0; i < this.nLibrosActuales; i++) {
+                if (this.libros[i] != null) {
+                    if (this.libros[i].getISBN().equalsIgnoreCase(libro.getISBN())) {
+                        index = i;
+                        break; //para eliminar la primera ocurrencia
+                    }
+                }
+            }
+
+            if(index != -1){
+                this.libros[index] = null;
+                resultado = true;
+
+                //Opcion 1 (Marco): Mover solo el ultimo
+                this.libros[index] = this.libros[nLibrosActuales- 1];
+                this.libros[nLibrosActuales- 1] = null;
+                this.nLibrosActuales--;
+
+//                //Opcion 2: recoloco todo el array
+//                for(int i = index +1; i < this.libros.length; i++) {
+//                    this.libros[i-1] = this.libros[i];
+//                }
+//                this.libros[this.libros.length-1] = null;
+//                this.nLibrosActuales--;
+            }
+        }
+        return  resultado;
+    }
+
+    public String infoBiblioteca(){
+        String infoBiblioteca = String.format("Bibilioteca - Nombre: %s, Direccion: %s , " +
+                        "Email: %s, Numero telefono: %s"
+                , this.nombre, this.direccion, this.email, this.nTelefono);
+
+        StringBuilder sb = new StringBuilder("Biblioteca: \n");
+        sb.append("Nombre: " + this.nombre + "\n");
+        sb.append("Direccion: " + this.direccion + "\n");
+        sb.append("Email: " + this.email + "\n");
+        sb.append("Numero telefono: " + this.nTelefono + "\n");
+
+        return sb.toString();
+        //return infoBiblioteca;
+    }
+
+    public String infoLibros(){
+        String infoLibro = "";
+        if(this.nLibrosActuales > 0){
+            for(int i = 0; i < this.nLibrosActuales; i++){
+                if(this.libros[i] != null){
+                    infoLibro += this.libros[i].infoLibro() + "\n";
+                }
+            }
+        }
+        else{
+            infoLibro = "No hay libros en la biblioteca";
+        }
+
+        return infoLibro;
+    }
 
     private void ampliarDimensionBiblioteca(){
         Libro[] librosAux = new Libro[this.libros.length + 5];
@@ -95,5 +189,6 @@ public class Biblioteca {
         }
         this.libros = librosAux;
     }
+
 
 }
